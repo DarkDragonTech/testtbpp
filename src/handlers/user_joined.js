ca60a84e468cc0a279f6253d35690af2e0d748e3
1024x2 <cod1nggam3rhd@gmail.com>
2019-09-19 18:24:52 +0100

    Added RGB colors to logging.

--- src/handlers/user_joined.js
+++ src/handlers/user_joined.js
@@ -1,14 +1,16 @@
 const User = require("../User.js");
-const { generateSystemMessage, updateUserList } = require("../util.js");
+const { generateSystemMessage, updateUserList, nickToANSI } = require("../util.js");
 
 module.exports = (socket, log, nick, color, style, password) => {
-  log(nick + " joined (" + socket.handshake.address + ")");
-
   let user = new User(socket, nick, color, style, password);
 
   if (socket.server.users[socket.id]) {
+    log(nickToANSI(socket.server.users[socket.id].nick, socket.server.users[socket.id].color) + " changed nick to " + nickToANSI(nick, color));
+
     socket.io.emit("user change nick", socket.server.users[socket.id].getSafeObject(), user.getSafeObject());
   } else {
+    log(nickToANSI(user.nick, user.color) + " joined (" + socket.handshake.address + ")");
+
     socket.io.emit("user joined", user.getSafeObject());
 
     socket.emit("message", generateSystemMessage(
