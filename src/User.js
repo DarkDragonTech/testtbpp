c58e78824b58dee2111703a9050f4b9056e73cec
1024x2 <cod1nggam3rhd@gmail.com>
2019-09-19 19:44:53 +0100

    oops

--- src/User.js
+++ src/User.js
@@ -1,11 +1,35 @@
 const he = require("he");
 
+// Thanks to ziad87 for this function
+function colorAlgo(name) {
+  const COLORS = [
+    "#e21400",
+    "#f78b00",
+    "#f8a700",
+    "#f78b00",
+    "#58dc00",
+    "#c3ff00",
+    "#a8f07a",
+    "#4ae8c4",
+    "#3b88eb",
+    "#ff00ff",
+    "#a700ff",
+    "#d300e7"
+  ];
+
+  let hash = 7;
+  for (let i = 0; i < name.length; i++) {
+    hash = n.charCodeAt(i) + (hash<<5) - hash;
+  }
+  return COLORS[Math.abs(hash % COLORS.length)];
+}
+
 class User {
   constructor(socket, nick, color, style, pass) {
     this.socket = socket;
 
-    this.nick = he.encode(nick || "");
-    this.color = color || "";
+    this.nick = he.encode(nick || "anonymous");
+    this.color = color || colorAlgo(this.nick);
     this.style = style || "";
     this.pass = pass || "";
   }
--- src/util.js
+++ src/util.js
@@ -1,37 +1,14 @@
 const rgba = require("color-rgba");
 
-// Thanks to ziad87 for this function
-function colorAlgo(name) {
-  const COLORS = [
-    "#e21400",
-    "#f78b00",
-    "#f8a700",
-    "#f78b00",
-    "#58dc00",
-    "#c3ff00",
-    "#a8f07a",
-    "#4ae8c4",
-    "#3b88eb",
-    "#ff00ff",
-    "#a700ff",
-    "#d300e7"
-  ];
-
-  let hash = 7;
-  for (let i = 0; i < name.length; i++) {
-    hash = n.charCodeAt(i) + (hash<<5) - hash;
-  }
-  return COLORS[Math.abs(hash % COLORS.length)];
-}
 
 module.exports = {
   generateMessage(user, msg) {
     if (user.getSafeObject) user = user.getSafeObject();
     return {
       date: Date.now(),
-      nick: user.nick || "anonymoose",
-      color: user.color || colorAlgo(),
-      style: user.style || ,
+      nick: user.nick,
+      color: user.color,
+      style: user.style,
       msg: msg
     }
   },
