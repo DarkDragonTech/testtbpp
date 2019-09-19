04f8edfc3b21913f95d243e493faf6ce854e6c09
DarkPhoenix10 <thedarkpheonix123@gmail.com>
2019-09-19 15:22:41 +0000

    make the http server work on nodejs 8 lol

--- src/HTTPServer.js
+++ src/HTTPServer.js
@@ -5,9 +5,10 @@ module.exports = (server) => {
   const app = express();
 
   app.get("/", async (req, res) => {
-    let file = await fs.promises.readFile(__dirname + "/../static/index.html", "utf-8");
-
-    res.send(file.replace(/{{{{{{PUTHOSTHEREPLSSSSS}}}}}}/, "//:" + server.port + "/"));
+    fs.readFile(__dirname + "/../static/index.html", (error, data) => {
+      if (error) throw error;
+      res.send(data.toString().replace(/{{{{{{PUTHOSTHEREPLSSSSS}}}}}}/, "//:" + server.port + "/"));
+    });
   });
 
   app.use(express.static(__dirname + "/../static/"));
