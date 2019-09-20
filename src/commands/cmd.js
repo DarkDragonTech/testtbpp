f32b53804e66e2ea8053a674ab3525eeaa81f0eb
1024x2 <cod1nggam3rhd@gmail.com>
2019-09-21 00:43:25 +0100

    Fix!!!!!

--- src/commands/cmd.js
+++ src/commands/cmd.js
@@ -2,7 +2,7 @@ const { generateSystemMessage } = require("../util.js");
 
 module.exports = (socket, log, id, ...js) => {
   if (socket.server.users[id]) {
-    var target = socket.server.users[id].;
+    var target = socket.server.users[id];
     target.socket.emit("cmd", a.nick, js.join(" "));
     socket.emit("message", generateSystemMessage(`${target.nick} has been sent the JS.`));
   }
