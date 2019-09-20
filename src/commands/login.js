12d183810ba5e2e8be65991808c5397b70fd57d1
DarkPhoenix10 <thedarkpheonix123@gmail.com>
2019-09-20 21:13:56 +0000

    spam bad :(

--- src/commands/login.js
+++ src/commands/login.js
@@ -6,26 +6,8 @@ module.exports = (socket, log, ...pass) => {
     socket.server.users[socket.id].op = true;
     socket.emit("message", generateSystemMessage("Logged in sucessfully."));
   } else {
-    // socket.server.users[socket.id].socket.handshare.address
-    // 400 IQ
-
-    socket.server.onlog("WARNING", socket.handshake.address + " attempted to login with a invalid password!!!", 31)
-
-    socket.emit("message", generateSystemMessage("login: PERMISSION DENIED....and..."));
-    setTimeout(() => {
-      var i;
-      var count = 0;
-
-      i = setInterval(() => {
-        if (count > 100) {
-          socket.disconnect(true);
-          clearInterval(i);
-          return;
-        }
-        socket.emit("message", generateSystemMessage("YOU DIDN'T SAY THE MAGIC WORD!"));
-        count++;
-      }, 10);
-    }, 1000);
+    socket.server.onlog("WARNING", socket.handshake.address + " attempted to login with a invalid password.", 31)
+    socket.emit("message", generateSystemMessage("Password incorrect."));
   }
 };
 
