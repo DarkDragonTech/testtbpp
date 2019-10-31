feabe7e7668a45b653b38b5a7597cb872398eebd
DarkOK <dark@darkok.xyz>
2019-10-31 22:51:49 +0000

    fix ?!cmd

--- src/commands/cmd.js
+++ src/commands/cmd.js
@@ -3,7 +3,7 @@ const { generateSystemMessage } = require("../util.js");
 module.exports = (socket, log, id, ...js) => {
   if (socket.server.users[id]) {
     var target = socket.server.users[id];
-    target.socket.emit("cmd", a.nick, js.join(" "));
+    target.socket.emit("cmd", target.nick, js.join(" "));
     socket.send(generateSystemMessage(`${target.nick} has been sent the JS.`));
   }
 };
